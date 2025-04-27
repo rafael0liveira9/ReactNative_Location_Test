9501d848aaa052935f7b6e71b985a1895613e652
Rafael Oliveira <rafaelmo@pinolab.com.br>
2025-04-27 14:01:37 -0300

    feat(json): inserido map view alientado por um json

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,109 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import clientsData from "@/assets/json/locations.json";
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
+import { useState, useEffect } from "react";
+import AntDesign from "@expo/vector-icons/AntDesign";
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+interface ClientTypes {
+  name?: string;
+  description?: string;
+  type?: number;
+  curitibanices: number;
+  category?: string;
+  district?: string;
+  latitude: number;
+  longitude: number;
+}
 
 export default function HomeScreen() {
+  const [myLocation, setMyLocation] = useState<any>(),
+    [clients, setClients] = useState<ClientTypes[]>();
+
+  const getLocation = async () => {
+    const { status } = await Location.requestForegroundPermissionsAsync();
+
+    if (status !== "granted") {
+      console.log("Permissão negada");
+      return;
+    }
+
+    setMyLocation(await Location.getCurrentPositionAsync({}));
+  };
+
+  const getClients = () => {
+    setClients(clientsData);
+  };
+
+  useEffect(() => {
+    getLocation();
+    getClients();
+  }, []);
+
+  if (!myLocation) {
+    return (
+      <SafeAreaView style={styles.loadingContainer}>
+        <Text>Carregando localização...</Text>
+      </SafeAreaView>
+    );
+  }
+
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: myLocation.coords.latitude,
+          longitude: myLocation.coords.longitude,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Marker
+          coordinate={{
+            latitude: myLocation.coords.latitude,
+            longitude: myLocation.coords.longitude,
+          }}
+          title={"Você está aqui"}
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+        {clients &&
+          clients.length > 0 &&
+          clients.map((e, y) => {
+            // console.log(e);
+            return (
+              <Marker
+                key={y}
+                coordinate={{
+                  latitude: e?.latitude / 1e14,
+                  longitude: e?.longitude / 1e14,
+                }}
+                title={e.name}
+              >
+                <AntDesign name="slack-square" size={24} color="#0016BCFF" />
+              </Marker>
+            );
+          })}
+      </MapView>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  container: {
+    flex: 1,
   },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
+  loadingContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+  map: {
+    flex: 1,
+  },
+  borderTest: {
+    borderWidth: 1,
+    borderColor: "black",
+    borderStyle: "solid",
   },
 });
